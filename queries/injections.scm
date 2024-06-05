1a39397b7840cf581f1ac5573614565c73dc2550
armonjam <>
2024-06-05 13:23:24 -0400

    use prefix to determine parsing language

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,3 +1,4 @@
-; TODO: use the content of the hom_name to get the language highlighting.
-((hom_body) @injection.content (#set! injection.language "tex") (#set! injection.include_children) )
+(homomorphism
+  name: (hom_name) @injection.language
+  body: (hom_body) @injection.content (#set! injection.include_children))
 ((hom_inner_block) @injection.content (#set! injection.language "ott") (#set! injection.include_children) )
