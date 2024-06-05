6127af95c5b06f860b2ccd881ff599bced5141be
armonjam <>
2024-06-05 14:45:50 -0400

    add support for tex-preamble highlighting

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,4 +2,7 @@
 (homomorphism
   name: (hom_name) @injection.language
   body: (hom_body) @injection.content (#set! injection.include_children))
+(homomorphism
+  name: (hom_name) @_hom_name (#eq? @_hom_name "tex-preamble")
+  body: (hom_body) @injection.content (#set! injection.language "tex") (#set! injection.include_children))
 ((hom_inner_block) @injection.content (#set! injection.language "ott") (#set! injection.include_children) )
