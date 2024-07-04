587c9bfb1a18f913a31fe29f9bedee48b441c562
armonjam <>
2024-07-04 19:01:01 -0400

    use better constants for delimiters

--- src/scanner.c
+++ src/scanner.c
@@ -13,81 +13,18 @@ enum TokenType {
 };
 
 typedef struct {
-    const int num_start_chars;
-    const int num_break_chars;
-    const uint32_t *start_chars;
-    const uint32_t *break_chars;
+    uint32_t left;
+    uint32_t right;
+    bool is_dots;
 } delim_exp;
 
-const uint32_t _HOM_INNER_CLOSE_START[] = {']', ']'};
-const uint32_t _HOM_INNER_OPEN_START[]  = {'[', '['};
-const uint32_t _HOM_CLOSE_START[]       = {'}', '}'};
-const uint32_t _COMP_OPEN_START[]       = {'<', '/'};
-const uint32_t _COMP_CLOSE_START[]       = {'/', '>'};
-const uint32_t _COMP_MIDDLE_START[]       = {'/', '/'};
-// dots
-const uint32_t _DOTS_2_START[] = {'.', '.'};
-const uint32_t _DOTS_2_BREAK[] = {'.'};
-const uint32_t _DOTS_3_START[] = {'.', '.', '.'};
-const uint32_t _DOTS_3_BREAK[] = {'.'};
-const uint32_t _DOTS_4_START[] = {'.', '.', '.', '.'};
-const uint32_t _DOTS_4_BREAK[] = {'.'};
-
-const delim_exp COMP_MIDDLE = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _COMP_MIDDLE_START,
-    .break_chars = {},
-};
-const delim_exp COMP_CLOSE = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _COMP_CLOSE_START,
-    .break_chars = {},
-};
-const delim_exp COMP_OPEN = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _COMP_OPEN_START,
-    .break_chars = {},
-};
-const delim_exp HOM_INNER_CLOSE = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _HOM_INNER_CLOSE_START,
-    .break_chars = {},
-};
-const delim_exp HOM_INNER_OPEN = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _HOM_INNER_OPEN_START,
-    .break_chars = {},
-};
-const delim_exp HOM_CLOSE = {
-    .num_start_chars = 2,
-    .num_break_chars = 0,
-    .start_chars = _HOM_CLOSE_START,
-    .break_chars = {},
-};
-const delim_exp DOTS_2 = {
-    .num_start_chars = 2,
-    .num_break_chars = 1,
-    .start_chars = _DOTS_2_START,
-    .break_chars = _DOTS_2_BREAK
-};
-const delim_exp DOTS_3 = {
-    .num_start_chars = 3,
-    .num_break_chars = 1,
-    .start_chars = _DOTS_3_START,
-    .break_chars = _DOTS_3_BREAK
-};
-const delim_exp DOTS_4 = {
-    .num_start_chars = 4,
-    .num_break_chars = 1,
-    .start_chars = _DOTS_4_START,
-    .break_chars = _DOTS_4_BREAK
-};
-
+const delim_exp HOM_INNER_CLOSE = {.left = ']', .right = ']', .is_dots = false};
+const delim_exp HOM_INNER_OPEN  = {.left = '[', .right = '[', .is_dots = false};
+const delim_exp HOM_CLOSE       = {.left = '}', .right = '}', .is_dots = false};
+const delim_exp COMP_OPEN       = {.left = '<', .right = '/', .is_dots = false};
+const delim_exp COMP_CLOSE      = {.left = '/', .right = '>', .is_dots = false};
+const delim_exp COMP_MIDDLE     = {.left = '/', .right = '/', .is_dots = false};
+const delim_exp DOTS            = {.left = '.', .right = '.', .is_dots = true};
 
 void *tree_sitter_ott_external_scanner_create(void) {
     return NULL;
@@ -156,25 +93,28 @@ static bool _is_delim_still_valid(
     delim_exp delim,
     int match_id
 ) {
-    if (match_id < delim.num_start_chars && lookahead != delim.start_chars[match_id]) {
-        return false;
-    }
-    if (match_id == delim.num_start_chars) {
-        for (int i = 0; i < delim.num_break_chars; ++i) {
-            if (delim.break_chars[i] == lookahead) {
-                return false;
-            }
-        }
-    }
-    return true;
+    if (match_id == 0 && lookahead != delim.left) { return false; }
+    if (match_id == 1 && lookahead != delim.right) { return false; }
+    if (match_id > 1) { return false; }
+    return  true;
 }
 
 static bool _is_delim_matched(
+    TSLexer *lexer,
     delim_exp delim,
     int match_id,
     bool is_valid
 ) {
-    return match_id >= delim.num_start_chars && is_valid;
+    if (match_id >= 1 && is_valid) {
+        if (delim.is_dots) {
+            // eat the remaining dots
+            int count = 1;
+            while (lexer->lookahead == '.') { lexer->advance(lexer, false); ++count; }
+            return count <= 4;
+        }
+        return true;
+    }
+    return false;
 }
 
 static bool _try_match(
@@ -196,7 +136,7 @@ static bool _try_match(
             if (!_is_delim_still_valid(lexer->lookahead, delims[delim_id], match_id)) {
                 valid_delims[delim_id] = false;
             }
-            if (_is_delim_matched(delims[delim_id], match_id, valid_delims[delim_id])) {
+            if (_is_delim_matched(lexer, delims[delim_id], match_id, valid_delims[delim_id])) {
                 return true;
             }
         }
@@ -246,7 +186,7 @@ bool tree_sitter_ott_external_scanner_scan(
     }
     if (valid_symbols[HOM_STRING]) {
         lexer->result_symbol = HOM_STRING;
-        delim_exp delims[] = { HOM_CLOSE, HOM_INNER_OPEN, DOTS_2, DOTS_3, DOTS_4 };
+        delim_exp delims[] = { HOM_CLOSE, HOM_INNER_OPEN, DOTS };
         return scan_string(lexer, 5, delims, false);
     }
     if (valid_symbols[HOM_INNER_STRING_RIGHT]) {
@@ -256,12 +196,12 @@ bool tree_sitter_ott_external_scanner_scan(
     }
     if (valid_symbols[HOM_INNER_STRING_LEFT]) {
         lexer->result_symbol = HOM_INNER_STRING_LEFT;
-        delim_exp delims[] = { HOM_INNER_CLOSE, DOTS_2, DOTS_3, DOTS_4 };
+        delim_exp delims[] = { HOM_INNER_CLOSE, DOTS };
         return scan_string(lexer, 4, delims, true);
     }
     if (valid_symbols[HOM_INNER_STRING_INIT]) {
         lexer->result_symbol = HOM_INNER_STRING_INIT;
-        delim_exp delims[] = { COMP_OPEN, HOM_INNER_CLOSE, DOTS_2, DOTS_3, DOTS_4 };
+        delim_exp delims[] = { COMP_OPEN, HOM_INNER_CLOSE, DOTS };
         return scan_string(lexer, 5, delims, true);
     }
     if (valid_symbols[HOM_INNER_COMP_LEFT]) {
